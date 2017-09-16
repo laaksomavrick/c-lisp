bb1e3219af0b41078e69d1e00023787a7be9adff
mlaakso@dev <laakso.mavrick@gmail.com>
2017-09-16 19:54:37 -0400

    eval num calcultions

--- parsing.c
+++ parsing.c
@@ -4,6 +4,38 @@
 
 #include <editline/readline.h>
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    
+    //base case
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    //operator is always 2nd child as per our grammar
+    char* op = t->children[1]->contents;
+
+    //store the next expr
+    long x = eval(t->children[2]);
+
+    //iterate remaining children
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+    
+}
+
 int main(int argc, char** argv) {
 
     mpc_parser_t* Number = mpc_new("number");
@@ -20,7 +52,7 @@ int main(int argc, char** argv) {
         ",
         Number, Operator, Expr, Clisp);
 
-    puts("C-LISP Version 1.0.0");
+    puts("c-lisp version 0.0.1");
     puts("Press CTRL+c to Exit\n");
 
     while (1) {
@@ -32,7 +64,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
 
         if (mpc_parse("<stdin>", input, Clisp, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
