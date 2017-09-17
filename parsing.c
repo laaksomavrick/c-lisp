82ca9dd1f654555fc52bdcb7c510ed2dd246b408
mlaakso@dev <laakso.mavrick@gmail.com>
2017-09-17 16:13:29 -0400

    err handling

--- parsing.c
+++ parsing.c
@@ -4,26 +4,87 @@
 
 #include <editline/readline.h>
 
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+//struct representing possible values
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+//consts for lval type
+enum { LVAL_NUM, LVAL_ERR };
+
+//consts for error type
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+//lval generators
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+//lval convenience print
+
+void lval_print(lval v) {
+
+    switch(v.type) {
+
+        case LVAL_NUM: printf("%li", v.num); break;
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) {
+                printf("ERROR! Division by zero");
+            } else if (v.err == LERR_BAD_OP) {
+                printf("ERROR! Invalid operation");                
+            } else if (v.err == LERR_BAD_NUM) {
+                printf("ERROR! Invalid number");                
+            }
+            break;
+    }
+
+}
+
+void lval_println(lval v) {
+    lval_print(v); putchar('\n');
+}
+
+lval eval_op(lval x, char* op, lval y) {
+
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }    
+
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) { 
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
     
     //base case
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);    
     }
 
     //operator is always 2nd child as per our grammar
     char* op = t->children[1]->contents;
 
     //store the next expr
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     //iterate remaining children
     int i = 3;
@@ -64,8 +125,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
 
         if (mpc_parse("<stdin>", input, Clisp, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
